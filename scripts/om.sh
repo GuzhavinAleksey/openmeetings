f4fa899caf3713f8016efabdddf5afcb9277cb6c
GuzhavinAleksey <32718008+GuzhavinAleksey@users.noreply.github.com>
2020-04-10 15:55:55 +0300

    Update om.sh

--- scripts/om.sh
+++ scripts/om.sh
@@ -1,4 +1,3 @@
- 
 #!/bin/bash
 # #############################################
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,8 +13,44 @@
 # limitations under the License.
 # #############################################
 
-export CATALINA_OPTS="-DDATA_DIR=${OM_DATA_DIR}"
+. ${work}/om_euser.sh
+echo "OM server of type ${OM_TYPE} will be run"
+CLASSES_HOME=${OM_HOME}/webapps/openmeetings/WEB-INF/classes
+if [ "${OM_TYPE}" == "min" ]; then
+	DB_CFG_HOME=${CLASSES_HOME}/META-INF
+	cp ${DB_CFG_HOME}/${OM_DB_TYPE}_persistence.xml ${DB_CFG_HOME}/persistence.xml
+	case ${OM_DB_TYPE} in
+		db2)
+			sed -i "s|localhost:50000/openmeet|${OM_DB_HOST}:${OM_DB_PORT}/${OM_DB_NAME}|g" ${DB_CFG_HOME}/persistence.xml
+		;;
+		mssql)
+			sed -i "s|localhost:1433;databaseName=openmeetings|${OM_DB_HOST}:${OM_DB_PORT};databaseName=${OM_DB_NAME}|g" ${DB_CFG_HOME}/persistence.xml
+		;;
+		mysql)
+			sed -i "s|localhost:3306/openmeetings|${OM_DB_HOST}:${OM_DB_PORT}/${OM_DB_NAME}|g" ${DB_CFG_HOME}/persistence.xml
+		;;
+		postgresql)
+			sed -i "s|localhost:5432/openmeetings|${OM_DB_HOST}:${OM_DB_PORT}/${OM_DB_NAME}|g" ${DB_CFG_HOME}/persistence.xml
+		;;
+	esac
+	sed -i "s/Username=/Username=${OM_DB_USER}/g; s/Password=/Password=${OM_DB_PASS}/g" ${DB_CFG_HOME}/persistence.xml
+	if [ ! -d "${OM_DATA_DIR}" ]; then
+		mkdir "${OM_DATA_DIR}"
+	fi
+	sed -i "s|ws://127.0.0.1:8888/kurento|${OM_KURENTO_WS_URL}|g" ${CLASSES_HOME}/applicationContext.xml
 
+	export CATALINA_OPTS="-DDATA_DIR=${OM_DATA_DIR}"
+else
+	service kurento-media-server start
+fi
+if [ -n "${TURN_URL}" ]; then
+	sed -i "s|p:turnUrl=\"\"|p:turnUrl=\"${TURN_URL}\"|g" ${CLASSES_HOME}/applicationContext.xml
+fi
+if [ -n "${TURN_USER}" ]; then
+	sed -i "s|p:turnUser=\"\"|p:turnUser=\"${TURN_USER}\"|g" ${CLASSES_HOME}/applicationContext.xml
+fi
+if [ -n "${TURN_PASS}" ]; then
+	sed -i "s|p:turnSecret=\"\"|p:turnSecret=\"${TURN_PASS}\"|g" ${CLASSES_HOME}/applicationContext.xml
 fi
 cd ${OM_HOME}
 sudo -u ${DAEMON_USER} ${OM_HOME}/bin/catalina.sh run
